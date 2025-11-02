79ef5c38a5a19a6a2859e8aa79af69d5e7e913a5
developOnion <smeyonion.nou@gmail.com>
2025-11-02 12:33:11 +0700

    Initial commit

---
+++ components/Main.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+
+export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getRandomImage() {
+    const randomImageUrl =
+      allMemes[Math.floor(Math.random() * allMemes.length)].url;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        imageUrl: randomImageUrl,
+      };
+    });
+  }
+
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+          />
+        </label>
+
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
+        </label>
+        <button onClick={getRandomImage}>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
+}
